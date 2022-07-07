925844967d59a671422acf113e6bfe634b4582f3
Vivek Gupta <30310079+VivekkrGupta@users.noreply.github.com>
2022-07-07 13:05:52 +0200

    updates employee not find exception message

--- src/main/java/com/digitech/springrestapi/Exceptions/EmployeeNotFoundException.java
+++ src/main/java/com/digitech/springrestapi/Exceptions/EmployeeNotFoundException.java
@@ -3,6 +3,6 @@ package com.digitech.springrestapi.Exceptions;
 public class EmployeeNotFoundException extends RuntimeException{
 
     public EmployeeNotFoundException(Long id){
-        super("Could not find employee " + id);
+        super("Could not find employee for " + id);
     }
 }
